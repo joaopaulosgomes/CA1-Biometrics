a5902848793fc836be3252753780951e8b84e526
joaopaulosgomes <72171415+joaopaulosgomes@users.noreply.github.com>
2021-04-10 00:14:58 +0100

    Co-Authors added
    Co-Authored-By: Jana <57003051+janamiranda@users.noreply.github.com>
    Co-Authored-By: deuzneves <75511287+deuzneves@users.noreply.github.com>

--- App.js
+++ App.js
@@ -1,21 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  SafeAreaView,
+  View,
+  Modal,
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+  Alert,
+  Platform
+} from "react-native";
+import * as LocalAuthentication from "expo-local-authentication";
+
+//funtions to be added: assync function, cancel modal or alert, all styles
+//to be modified, all interface
+//files to be added: TransactionScreen.js
 
 export default function App() {
+
+  const [isModalVisible, setIsModalVisible] = useState(true);
+
+  async function authenticate() {
+
+    
+ 
+  }
+
+  Platform.OS === "ios" && authenticate();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.loginText}
+        >Wellcome to YourBank
+      </Text>
+      <Text style={styles.loginText}
+        >Press the button to log in</Text>
+      <TouchableOpacity style={styles.button}
+      >
+        <Text>Enter</Text>
+      </TouchableOpacity>
+
+      
+      
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: "#19181f",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: "center",
+    alignItems: "center"
+  }
 });
